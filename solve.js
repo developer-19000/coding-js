a5734914d1802dba59fda81c449c8ee39ed63d79
Reza <reza.linusx@gmail.com>
2024-04-26 18:29:16 +0330

    parrotTrouble

--- solve.js
+++ solve.js
@@ -26,3 +26,15 @@ function diff21(n) {
     return diff;
   }
 }
+
+// 5
+function parrotTrouble(talking, hour) {
+  if (!talking) {
+    return false;
+  } else {
+    if (hour < 7 || hour > 20) {
+      return true;
+    }
+    return false;
+  }
+}
