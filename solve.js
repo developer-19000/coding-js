4b26d68b67f674733a09c1714b2842b8d31d87e4
Reza <reza.linusx@gmail.com>
2024-05-08 17:19:20 +0330

    everyNth

--- solve.js
+++ solve.js
@@ -256,3 +256,12 @@ function endUp(str) {
   }
 }
 
+// 30
+function everyNth(str, n) {
+  let result = "";
+  for (let i = 0; i < str.length; i += n) {
+    result += str[i];
+  }
+  return result;
+}
+
