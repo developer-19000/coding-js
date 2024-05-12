7a35d4746092568151a485c1c5566baa78c7b6d6
Reza <reza.linusx@gmail.com>
2024-05-12 16:30:22 +0330

    Add comments to customToUpperCase

--- implements.js
+++ implements.js
@@ -11,10 +11,16 @@ function customCharAt(str, index) {
 function customToUpperCase(str) {
   let result = "";
   for (let char of str) {
+    // get the ASCII code of the current character
     let charCode = char.charCodeAt(0);
+
+    // Check if the ASCII code falls in the range of 97 to 122 (if it does , it is a lowercase letter)
     if (charCode >= 97 && charCode <= 122) {
+      // if it is lowercase , we subtract it by 32 to get the uppercase form of the letter
       charCode -= 32;
     }
+
+    // using this method we can find the character of certain ASCII code number
     result += String.fromCharCode(charCode);
   }
   return result;
