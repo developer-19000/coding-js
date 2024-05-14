5bd15add797f544d13556d51620c071c4d3b73ef
Reza <reza.linusx@gmail.com>
2024-05-14 17:13:17 +0330

    array123

--- solve.js
+++ solve.js
@@ -353,3 +353,13 @@ function arrayFront9(nums) {
   return false;
 }
 
+// 40
+function array123(nums) {
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] === 1 && nums[i + 1] === 2 && nums[i + 2] === 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
