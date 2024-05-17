7e839d97c245217f5393eee7508283e62d98de01
Reza <reza.linusx@gmail.com>
2024-05-17 11:50:59 +0330

    array667

--- solve.js
+++ solve.js
@@ -411,3 +411,14 @@ function stringYak(str) {
   return result;
 }
 
+// 45
+function array667(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 6 && (nums[i + 1] === 6 || nums[i + 1] === 7)) {
+      count++;
+    }
+  }
+  return count;
+}
+
