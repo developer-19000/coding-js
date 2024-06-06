cbda00bbf6b7c80218ba59c6df3c0a6c5d37267c
Linus <reza.linusx@gmail.com>
2024-06-06 14:09:49 +0330

    countHi

--- solve.js
+++ solve.js
@@ -656,4 +656,14 @@ function doubleChar(str) {
   return result;
 }
 
+// 80
+function countHi(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "h" && str[i + 1] === "i") {
+      count++;
+    }
+  }
+  return count;
+}
 
