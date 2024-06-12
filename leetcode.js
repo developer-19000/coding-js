1558592991091d4480b2a6add09916056475aac7
Linus <reza.linusx@gmail.com>
2024-06-12 09:52:42 +0330

    Solve longestCommonPrefix

--- leetcode.js
+++ leetcode.js
@@ -56,3 +56,18 @@ var romanToInt = function (s) {
   return result;
 };
 
+// 14 - Longest Common Prefix (https://leetcode.com/problems/longest-common-prefix/description/)
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 0) return "";
+  strs.sort((a, b) => b.length - a.length);
+  const shortest = strs.pop();
+
+  let i = 0;
+  for (; i <= shortest.length; i++) {
+    const prefix = shortest.substring(0, i + 1);
+    if (!strs.every((str) => str.startsWith(prefix))) {
+      break;
+    }
+  }
+  return shortest.substring(0, i);
+};
