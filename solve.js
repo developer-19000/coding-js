7a9c726a0b7eb7416b6228788167b51b3ee4b7b8
Linus <reza.linusx@gmail.com>
2024-06-15 08:50:02 +0330

    plusOut

--- solve.js
+++ solve.js
@@ -850,3 +850,16 @@ function starOut(str) {
 }
 
 // 100
+function plusOut(str, word) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str.slice(i, i + word.length) === word) {
+      result += word;
+      i += word.length - 1;
+    } else {
+      result += "+";
+    }
+  }
+  return result;
+}
+
