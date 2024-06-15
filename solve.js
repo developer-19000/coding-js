160adc5a553a19fbf94f6cf2333ee61f4720587e
Linus <reza.linusx@gmail.com>
2024-06-15 08:35:38 +0330

    zipZap

--- solve.js
+++ solve.js
@@ -824,3 +824,16 @@ function oneTwo(str) {
   return result;
 }
 
+// 98
+function zipZap(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "z" && str[i + 2] === "p") {
+      result += "zp";
+      i += 2;
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
