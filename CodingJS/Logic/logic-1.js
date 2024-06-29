694d6e3b9113a6c181233ec9dc1377cf9792702f
Linus <reza.linusx@gmail.com>
2024-06-29 19:56:30 +0330

    dateFashion

--- CodingJS/Logic/logic-1.js
+++ CodingJS/Logic/logic-1.js
@@ -6,3 +6,11 @@ function cigarParty(cigars, isWeekend) {
     return cigars >= 40 && cigars <= 60;
   }
 }
+
+// 114
+function dateFashion(you, date) {
+  if (you <= 2 || date <= 2) return 0;
+  if (you >= 8 || date >= 8) return 2;
+  return 1;
+}
+
