845ddf7b43cb5ffe1d1acac584be20cdb6feb1ae
Reza Br <reza.linusx@gmail.com>
2024-09-10 17:14:49 +0330

    implement push method in LinkedList

--- DSA/LinkedList/linkedList.js
+++ DSA/LinkedList/linkedList.js
@@ -21,4 +21,24 @@ class LinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  // add a new node to the end of the list
+  push(value) {
+    /*
+              first we check the value 
+              if the value is not valid we return
+              then we create a new node
+              if we don't have a head , meaning the list is empty we set the head and tail to the new node
+              if we do have a head we link the tail to the new node and update the tail to the new node
+          */
+    if (value === undefined || value === null) return;
+    let node = new Node(value, null);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+  }
 }
