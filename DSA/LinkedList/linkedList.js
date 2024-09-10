1f676a62cc34060ce92ad7c852f5f8c6d442c59c
Reza Br <reza.linusx@gmail.com>
2024-09-10 17:14:14 +0330

    create Node class

---
+++ DSA/LinkedList/linkedList.js
@@ -0,0 +1,11 @@
+/*
+    first we create a node class
+    each node has a value and a pointer to the next node (look up LinkedList.md for explanation)
+*/
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
