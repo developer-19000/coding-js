89eb31843485e41cef06d0a2b52abeb3e246c14a
Reza <reza.linusx@gmail.com>
2025-06-07 13:02:40 +0330

    fizzString2

--- CodingJS/Logic/logic-1.js
+++ CodingJS/Logic/logic-1.js
@@ -154,3 +154,11 @@ function fizzString(str) {
   if (str.endsWith("b")) return "Buzz";
   return str;
 }
+
+// 132
+function fizzString2(n) {
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz!";
+  if (n % 3 === 0) return "Fizz!";
+  if (n % 5 === 0) return "Buzz!";
+  return `${n}!`;
+}
