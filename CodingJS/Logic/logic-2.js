1c229ba972aae3304689d03b5a33b0ce5db81165
null <reza.linusx@gmail.com>
2025-07-05 17:39:24 +0330

    makeBricks

---
+++ CodingJS/Logic/logic-2.js
@@ -0,0 +1,30 @@
+// 145 - first solution
+function makeBricks(small, big, goal) {
+  for (let i = 1; i <= small; i++) {
+    let inchesLeft = goal - i;
+    if (inchesLeft === 0) return true;
+    if (inchesLeft % 5 === 0 && inchesLeft / 5 <= big) return true;
+  }
+
+  for (let i = 1; i <= big; i++) {
+    let inchesLeft = goal - i * 5;
+    if (inchesLeft === 0) return true;
+    if (inchesLeft <= small && inchesLeft >= 0) return true;
+  }
+
+  return false;
+}
+
+// better solution
+function makeBricks2(small, big, goal) {
+  // how many bigs i can use.
+  let bigsIcanUse = Math.min(Math.floor(goal / 5), big);
+  goal = goal - bigsIcanUse * 5;
+  if (goal === 0 || goal <= small) return true;
+  return false;
+}
+
+// even better solution
+function makeBricks3(small, big, goal) {
+  return goal - Math.min(Math.floor(goal / 5), big) * 5 <= small;
+}
