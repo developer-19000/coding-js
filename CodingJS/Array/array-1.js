14df0643e0bd3d7ed078be71fc022779efa5b835
player <reza.linusx@gmail.com>
2025-07-09 14:30:50 +0330

    sameFirstLast

--- CodingJS/Array/array-1.js
+++ CodingJS/Array/array-1.js
@@ -2,3 +2,8 @@
 function firstLast6(nums) {
   return nums[0] === 6 || nums[nums.length - 1] === 6;
 }
+
+// 154
+function sameFirstLast(nums) {
+  return nums[0] === nums[nums.length - 1] && nums.length >= 1;
+}
