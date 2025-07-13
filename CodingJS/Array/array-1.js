4fc8fe84b08918061e1f4c8a4b2812bcbf1a03d2
player <reza.linusx@gmail.com>
2025-07-13 18:05:35 +0330

    front11 without built in methods

--- CodingJS/Array/array-1.js
+++ CodingJS/Array/array-1.js
@@ -212,3 +212,10 @@ function make2(a, b) {
 function front11(a, b) {
   return [...a.slice(0, 1), ...b.slice(0, 1)];
 }
+
+function front11_2(a, b) {
+  let result = [];
+  if (a.length > 0) result.push(a[0]);
+  if (b.length > 0) result.push(b[0]);
+  return result;
+}
