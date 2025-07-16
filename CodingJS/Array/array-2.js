34c033bda72c00380e5043606fabe6e2fa896134
player <reza.linusx@gmail.com>
2025-07-16 07:48:24 +0330

    has22

--- CodingJS/Array/array-2.js
+++ CodingJS/Array/array-2.js
@@ -111,3 +111,13 @@ function sum67(nums) {
   }
   return sum;
 }
+
+// 185
+function has22(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 2 && nums[i + 1] === 2) {
+      return true;
+    }
+  }
+  return false
+}
