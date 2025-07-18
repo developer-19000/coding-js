74c2d705fe93844bc8f36d45b15c14d1c02f173f
player <reza.linusx@gmail.com>
2025-07-18 18:15:01 +0330

    isEverywhere

--- CodingJS/Array/array-2.js
+++ CodingJS/Array/array-2.js
@@ -204,3 +204,10 @@ function noNumber(arr, number) {
   return true;
 }
 
+// 194
+function isEverywhere(nums, val) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== val && nums[i + 1] !== val) return false;
+  }
+  return true;
+}
