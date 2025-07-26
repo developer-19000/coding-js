0988a7e921b6ec8dc59672882c77b8db8f8dc6f0
player <reza.linusx@gmail.com>
2025-07-26 09:31:31 +0330

    fizzBuzz

--- CodingJS/Array/array-2.js
+++ CodingJS/Array/array-2.js
@@ -522,3 +522,20 @@ function evenOdd(nums) {
   }
   return result;
 }
+
+// 214
+function fizzBuzz(start, end) {
+  let result = [];
+  for (let i = start; i < end; i++) {
+    if (i % 15 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(String(i));
+    }
+  }
+  return result;
+}
