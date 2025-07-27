98be50c4146dea40089844790f5f1fbfd26ac0db
player <reza.linusx@gmail.com>
2025-07-27 11:28:51 +0330

    maxSpan

---
+++ CodingJS/Array/array-3.js
@@ -0,0 +1,17 @@
+// 215
+function maxSpan(nums) {
+  if (nums.length === 1) return 1;
+  let largestSpan = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let currentSpan = 1;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] === nums[i]) {
+        currentSpan = j - i + 1;
+      }
+      if (currentSpan > largestSpan) {
+        largestSpan = currentSpan;
+      }
+    }
+  }
+  return largestSpan;
+}
